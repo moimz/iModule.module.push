700f74f36d1955df707da55f934580cd61625fd2
arzzcom <arzz@arzz.com>
2018-02-23 23:53:12 +0900

    아이모듈 구버전 코드 업데이트

--- ModulePush.class.php
+++ ModulePush.class.php
@@ -7,11 +7,12 @@
  * @file /modules/keyword/ModulePush.class.php
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
- * @version 3.0.0.160903
+ * @version 3.0.0
+ * @modified 2018. 2. 23.
  */
 class ModulePush {
 	/**
-	 * iModule core 와 Module core 클래스
+	 * iModule 및 Module 코어클래스
 	 */
 	private $IM;
 	private $Module;
@@ -19,8 +20,10 @@ class ModulePush {
 	/**
 	 * DB 관련 변수정의
 	 *
+	 * @private object $DB DB접속객체
 	 * @private string[] $table DB 테이블 별칭 및 원 테이블명을 정의하기 위한 변수
 	 */
+	private $DB;
 	private $table;
 	
 	/**
@@ -35,12 +38,15 @@ class ModulePush {
 	/**
 	 * class 선언
 	 *
-	 * @param iModule $IM iModule core class
-	 * @param Module $Module Module core class
+	 * @param iModule $IM iModule 코어클래스
+	 * @param Module $Module Module 코어클래스
 	 * @see /classes/iModule.class.php
 	 * @see /classes/Module.class.php
 	 */
 	function __construct($IM,$Module) {
+		/**
+		 * iModule 및 Module 코어 선언
+		 */
 		$this->IM = $IM;
 		$this->Module = $Module;
 		
@@ -75,7 +81,8 @@ class ModulePush {
 	 * @return DB $DB
 	 */
 	function db() {
-		return $this->IM->db($this->Module->getInstalled()->database);
+		if ($this->DB == null || $this->DB->ping() === false) $this->DB = $this->IM->db($this->getModule()->getInstalled()->database);
+		return $this->DB;
 	}
 	
 	/**
@@ -89,26 +96,151 @@ class ModulePush {
 	}
 	
 	/**
-	 * 사이트 외부에서 현재 모듈의 API를 호출하였을 경우, API 요청을 처리하기 위한 함수로 API 실행결과를 반환한다.
+	 * URL 을 가져온다.
+	 *
+	 * @param string $view
+	 * @param string $idx
+	 * @return string $url
+	 */
+	function getUrl($view=null,$idx=null) {
+		return $this->IM->getUrl(null,null,$view,$idx);
+	}
+	
+	/**
+	 * view 값을 가져온다.
+	 *
+	 * @return string $view
+	 */
+	function getView() {
+		return $this->IM->getView();
+	}
+	
+	/**
+	 * idx 값을 가져온다.
+	 *
+	 * @return string $idx
+	 */
+	function getIdx() {
+		return $this->IM->getIdx();
+	}
+	
+	/**
+	 * [코어] 사이트 외부에서 현재 모듈의 API를 호출하였을 경우, API 요청을 처리하기 위한 함수로 API 실행결과를 반환한다.
 	 * 소스코드 관리를 편하게 하기 위해 각 요쳥별로 별도의 PHP 파일로 관리한다.
 	 *
+	 * @param string $protocol API 호출 프로토콜 (get, post, put, delete)
 	 * @param string $api API명
+	 * @param any $idx API 호출대상 고유값
+	 * @param object $params API 호출시 전달된 파라메터
 	 * @return object $datas API처리후 반환 데이터 (해당 데이터는 /api/index.php 를 통해 API호출자에게 전달된다.)
 	 * @see /api/index.php
-	 * @todo 외부에서 알림을 보낼 수 있는 API 제공
 	 */
-	function getApi($api) {
+	function getApi($protocol,$api,$idx=null,$params=null) {
 		$data = new stdClass();
-		$values = new stdClass();
+		
+		$values = (object)get_defined_vars();
+		$this->IM->fireEvent('beforeGetApi',$this->getModule()->getName(),$api,$values);
 		
 		/**
 		 * 모듈의 api 폴더에 $api 에 해당하는 파일이 있을 경우 불러온다.
 		 */
-		if (is_file($this->Module->getPath().'/api/'.$api.'.php') == true) {
-			INCLUDE $this->Module->getPath().'/api/'.$api.'.php';
+		if (is_file($this->getModule()->getPath().'/api/'.$api.'.'.$protocol.'.php') == true) {
+			INCLUDE $this->getModule()->getPath().'/api/'.$api.'.'.$protocol.'.php';
 		}
 		
+		unset($values);
+		$values = (object)get_defined_vars();
+		$this->IM->fireEvent('afterGetApi',$this->getModule()->getName(),$api,$values,$data);
+		
 		return $data;
+	}
+	
+	/**
+	 * [사이트관리자] 모듈 관리자패널 구성한다.
+	 *
+	 * @return string $panel 관리자패널 HTML
+	 */
+	function getAdminPanel() {
+		/**
+		 * 설정패널 PHP에서 iModule 코어클래스와 모듈코어클래스에 접근하기 위한 변수 선언
+		 */
+		$IM = $this->IM;
+		$Module = $this;
+		
+		ob_start();
+		INCLUDE $this->getModule()->getPath().'/admin/index.php';
+		$panel = ob_get_contents();
+		ob_end_clean();
+		
+		return $panel;
+	}
+	
+	/**
+	 * [사이트관리자] 모듈의 전체 컨텍스트 목록을 반환한다.
+	 *
+	 * @return object $lists 전체 컨텍스트 목록
+	 */
+	function getContexts() {
+		$lists = array();
+		foreach ($this->getText('context') as $context=>$title) {
+			$lists[] = array('context'=>$context,'title'=>$title);
+		}
+		
+		return $lists;
+	}
+	
+	/**
+	 * 특정 컨텍스트에 대한 제목을 반환한다.
+	 *
+	 * @param string $context 컨텍스트명
+	 * @return string $title 컨텍스트 제목
+	 */
+	function getContextTitle($context) {
+		return $this->getText('context/'.$context);
+	}
+	
+	/**
+	 * [사이트관리자] 모듈의 컨텍스트 환경설정을 구성한다.
+	 *
+	 * @param object $site 설정대상 사이트
+	 * @param string $context 설정대상 컨텍스트명
+	 * @return object[] $configs 환경설정
+	 */
+	function getContextConfigs($site,$context) {
+		$configs = array();
+		
+		$templet = new stdClass();
+		$templet->title = $this->IM->getText('text/templet');
+		$templet->name = 'templet';
+		$templet->type = 'select';
+		$templet->data = array();
+		
+		$templet->data[] = array('#',$this->getText('admin/configs/form/default_setting'));
+		
+		$templets = $this->getModule()->getTemplets();
+		for ($i=0, $loop=count($templets);$i<$loop;$i++) {
+			$templet->data[] = array($templets[$i]->getName(),$templets[$i]->getTitle().' ('.$templets[$i]->getDir().')');
+		}
+		
+		$templet->value = count($templet->data) > 0 ? $templet->data[0][0] : '#';
+		$configs[] = $templet;
+		
+		return $configs;
+	}
+	
+	/**
+	 * 사이트맵에 나타날 뱃지데이터를 생성한다.
+	 *
+	 * @param string $context 컨텍스트종류
+	 * @param object $configs 사이트맵 관리를 통해 설정된 페이지 컨텍스트 설정
+	 * @return object $badge 뱃지데이터 ($badge->count : 뱃지숫자, $badge->latest : 뱃지업데이트 시각(UNIXTIME), $badge->text : 뱃지텍스트)
+	 * @todo check count information
+	 */
+	function getContextBadge($context,$config) {
+		/**
+		 * null 일 경우 뱃지를 표시하지 않는다.
+		 */
+		return null;
 	}
 	
 	/**
@@ -116,77 +248,229 @@ class ModulePush {
 	 * 코드에 해당하는 문자열이 없을 경우 1차적으로 package.json 에 정의된 기본언어셋의 텍스트를 반환하고, 기본언어셋 텍스트도 없을 경우에는 코드를 그대로 반환한다.
 	 *
 	 * @param string $code 언어코드
+	 * @param string $replacement 일치하는 언어코드가 없을 경우 반환될 메세지 (기본값 : null, $code 반환)
 	 * @return string $language 실제 언어셋 텍스트
 	 */
-	function getLanguage($code) {
+	function getText($code,$replacement=null) {
 		if ($this->lang == null) {
-			if (file_exists($this->Module->getPath().'/languages/'.$this->IM->language.'.json') == true) {
-				$this->lang = json_decode(file_get_contents($this->Module->getPath().'/languages/'.$this->IM->language.'.json'));
-				if ($this->IM->language != $this->Module->getPackage()->language) {
-					$this->oLang = json_decode(file_get_contents($this->Module->getPath().'/languages/'.$this->Module->getPackage()->language.'.json'));
+			if (is_file($this->getModule()->getPath().'/languages/'.$this->IM->language.'.json') == true) {
+				$this->lang = json_decode(file_get_contents($this->getModule()->getPath().'/languages/'.$this->IM->language.'.json'));
+				if ($this->IM->language != $this->getModule()->getPackage()->language && is_file($this->getModule()->getPath().'/languages/'.$this->getModule()->getPackage()->language.'.json') == true) {
+					$this->oLang = json_decode(file_get_contents($this->getModule()->getPath().'/languages/'.$this->getModule()->getPackage()->language.'.json'));
 				}
-			} else {
-				$this->lang = json_decode(file_get_contents($this->Module->getPath().'/languages/'.$this->Module->getPackage()->language.'.json'));
+			} elseif (is_file($this->getModule()->getPath().'/languages/'.$this->getModule()->getPackage()->language.'.json') == true) {
+				$this->lang = json_decode(file_get_contents($this->getModule()->getPath().'/languages/'.$this->getModule()->getPackage()->language.'.json'));
 				$this->oLang = null;
 			}
 		}
 		
+		$returnString = null;
 		$temp = explode('/',$code);
-		if (count($temp) == 1) {
-			return isset($this->lang->$code) == true ? $this->lang->$code : ($this->oLang != null && isset($this->oLang->$code) == true ? $this->oLang->$code : $code);
-		} else {
-			$string = $this->lang;
-			for ($i=0, $loop=count($temp);$i<$loop;$i++) {
-				if (isset($string->{$temp[$i]}) == true) {
-					$string = $string->{$temp[$i]};
-				} else {
-					$string = null;
-					break;
-				}
+		
+		$string = $this->lang;
+		for ($i=0, $loop=count($temp);$i<$loop;$i++) {
+			if (isset($string->{$temp[$i]}) == true) {
+				$string = $string->{$temp[$i]};
+			} else {
+				$string = null;
+				break;
 			}
-			
-			if ($string != null) return $string;
-			if ($this->oLang == null) return $code;
-			
+		}
+		
+		if ($string != null) {
+			$returnString = $string;
+		} elseif ($this->oLang != null) {
 			if ($string == null && $this->oLang != null) {
 				$string = $this->oLang;
 				for ($i=0, $loop=count($temp);$i<$loop;$i++) {
-					if (isset($string->{$temp[$i]}) == true) $string = $string->{$temp[$i]};
-					return $code;
+					if (isset($string->{$temp[$i]}) == true) {
+						$string = $string->{$temp[$i]};
+					} else {
+						$string = null;
+						break;
+					}
 				}
 			}
-			return $string;
+			
+			if ($string != null) $returnString = $string;
 		}
+		
+		$this->IM->fireEvent('afterGetText',$this->getModule()->getName(),$code,$returnString);
+		
+		/**
+		 * 언어셋 텍스트가 없는경우 iModule 코어에서 불러온다.
+		 */
+		if ($returnString != null) return $returnString;
+		elseif (in_array(reset($temp),array('text','button','action')) == true) return $this->IM->getText($code,$replacement);
+		else return $replacement == null ? $code : $replacement;
 	}
 	
 	/**
-	 * Get page context
+	 * 상황에 맞게 에러코드를 반환한다.
 	 *
-	 * @param string $container linked context code (signup, account, modify, ... etc)
-	 * @return string $context context html code
+	 * @param string $code 에러코드
+	 * @param object $value(옵션) 에러와 관련된 데이터
+	 * @param boolean $isRawData(옵션) RAW 데이터 반환여부
+	 * @return string $message 에러 메세지
 	 */
-	function getContext($container,$config=null) {
-		$context = '';
-		$values = new stdClass();
+	function getErrorText($code,$value=null,$isRawData=false) {
+		$message = $this->getText('error/'.$code,$code);
+		if ($message == $code) return $this->IM->getErrorText($code,$value,null,$isRawData);
 		
-		switch ($container) {
+		$description = null;
+		switch ($code) {
+			default :
+				if (is_object($value) == false && $value) $description = $value;
+		}
+		
+		$error = new stdClass();
+		$error->message = $message;
+		$error->description = $description;
+		$error->type = 'BACK';
+		
+		if ($isRawData === true) return $error;
+		else return $this->IM->getErrorText($error);
+	}
+	
+	/**
+	 * 템플릿 정보를 가져온다.
+	 *
+	 * @param string $this->getTemplet($configs) 템플릿명
+	 * @return string $package 템플릿 정보
+	 */
+	function getTemplet($templet=null) {
+		$templet = $templet == null ? '#' : $templet;
+		
+		/**
+		 * 사이트맵 관리를 통해 설정된 페이지 컨텍스트 설정일 경우
+		 */
+		if (is_object($templet) == true) {
+			$templet_configs = $templet !== null && isset($templet->templet_configs) == true ? $templet->templet_configs : null;
+			$templet = $templet !== null && isset($templet->templet) == true ? $templet->templet : '#';
+		} else {
+			$templet_configs = null;
+		}
+		
+		/**
+		 * 템플릿명이 # 이면 모듈 기본설정에 설정된 템플릿을 사용한다.
+		 */
+		if ($templet == '#') {
+			$templet = $this->getModule()->getConfig('templet');
+			$templet_configs = $this->getModule()->getConfig('templet_configs');
+		}
+		
+		return $this->getModule()->getTemplet($templet,$templet_configs);
+	}
+	
+	/**
+	 * 모듈 외부컨테이너를 가져온다.
+	 *
+	 * @param string $container 컨테이너명
+	 * @return string $html 컨텍스트 HTML
+	 */
+	function getContainer($container) {
+		$html = $this->getContext($container);
+		
+		$this->IM->addHeadResource('style',$this->getModule()->getDir().'/styles/container.css');
+		
+		$this->IM->removeTemplet();
+		$footer = $this->IM->getFooter();
+		$header = $this->IM->getHeader();
+		
+		return $header.$html.$footer;
+	}
+	
+	/**
+	 * 페이지 컨텍스트를 가져온다.
+	 *
+	 * @param string $qid 문의게시판 ID
+	 * @param object $configs 사이트맵 관리를 통해 설정된 페이지 컨텍스트 설정
+	 * @return string $html 컨텍스트 HTML
+	 */
+	function getContext($context,$config=null) {
+		/**
+		 * 컨텍스트 컨테이너를 설정한다.
+		 */
+		$html = PHP_EOL.'<!-- PUSH MODULE -->'.PHP_EOL.'<div data-role="context" data-type="module" data-module="'.$this->getModule()->getName().'" data-context="'.$context.'">'.PHP_EOL;
+		
+		/**
+		 * 컨텍스트 헤더
+		 */
+		$html.= $this->getHeader($context,$configs);
+		
+		/**
+		 * 컨테이너 종류에 따라 컨텍스트를 가져온다.
+		 */
+		switch ($context) {
 			case 'list' :
-				$context = $this->getListContext($config);
+				$html.= $this->getListContext($config);
 				break;
 		}
 		
-		$this->IM->fireEvent('afterGetContext','push',$container,null,null,$context);
+		/**
+		 * 컨텍스트 푸터
+		 */
+		$html.= $this->getFooter($context,$configs);
 		
-		return $context;
+		/**
+		 * 컨텍스트 컨테이너를 설정한다.
+		 */
+		$html.= PHP_EOL.'</div>'.PHP_EOL.'<!--// PUSH MODULE -->'.PHP_EOL;
+		
+		return $html;
 	}
 	
 	/**
-	 * Get list context
+	 * 컨텍스트 헤더를 가져온다.
 	 *
-	 * @param object $config context's config
-	 * @return string $context context's html
+	 * @param string $qid 문의게시판 ID
+	 * @param object $configs 사이트맵 관리를 통해 설정된 페이지 컨텍스트 설정
+	 * @return string $html 컨텍스트 HTML
 	 */
-	function getListContext($config) {
+	function getHeader($context,$configs=null) {
+		/**
+		 * 템플릿파일을 호출한다.
+		 */
+		return $this->getTemplet($configs)->getHeader(get_defined_vars());
+	}
+	
+	/**
+	 * 컨텍스트 푸터를 가져온다.
+	 *
+	 * @param string $context 컨테이너 종류
+	 * @param object $configs 사이트맵 관리를 통해 설정된 페이지 컨텍스트 설정
+	 * @return string $html 컨텍스트 HTML
+	 */
+	function getFooter($context,$configs=null) {
+		/**
+		 * 템플릿파일을 호출한다.
+		 */
+		return $this->getTemplet($configs)->getFooter(get_defined_vars());
+	}
+	
+	/**
+	 * 에러메세지를 반환한다.
+	 *
+	 * @param string $code 에러코드 (에러코드는 iModule 코어에 의해 해석된다.)
+	 * @param object $value 에러코드에 따른 에러값
+	 * @return $html 에러메세지 HTML
+	 */
+	function getError($code,$value=null) {
+		/**
+		 * iModule 코어를 통해 에러메세지를 구성한다.
+		 */
+		$error = $this->getErrorText($code,$value,true);
+		return $this->IM->getError($error);
+	}
+	
+	/**
+	 * 전체알림목록 컨텍스트를 가져온다.
+	 *
+	 * @param object $confgs 사이트맵 관리를 통해 설정된 페이지 컨텍스트 설정
+	 * @return $html 컨텍스트 HTML
+	 */
+	function getListContext($configs=null) {
+		/*
 		ob_start();
 		
 		if (preg_match('/\.php$/',$config->templet) == true) {
@@ -247,6 +531,7 @@ class ModulePush {
 		ob_end_clean();
 		
 		return $context;
+		*/
 	}
 	
 	function getPushCount($type='ALL') {
@@ -257,18 +542,42 @@ class ModulePush {
 		return $check->count();
 	}
 	
-	function sendPush($target,$module,$code,$fromcode,$content=array()) {
-		if (is_numeric($target) == true) { // for member
-			$check = $this->db()->select($this->table->push)->where('midx',$target)->where('module',$module)->where('code',$code)->where('fromcode',$fromcode)->where('is_check','FALSE')->getOne();
-			if ($check == null) {
-				$this->db()->insert($this->table->push,array('midx'=>$target,'module'=>$module,'code'=>$code,'fromcode'=>$fromcode,'content'=>json_encode(array($content),JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK),'reg_date'=>time()))->execute();
-			} else {
-				$contents = json_decode($check->content);
-				$contents[] = $content;
-				$this->db()->update($this->table->push,array('content'=>json_encode($contents,JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK),'reg_date'=>time()))->where('midx',$target)->where('module',$module)->where('code',$code)->where('fromcode',$fromcode)->execute();
-			}
-		} else { // for email
+	/**
+	 * 알림메세지를 전송한다.
+	 *
+	 * @param int/string $target 알림을 받는사람 (회원고유번호 또는 이메일)
+	 * @param string $module 알림메세지가 발생한 대상모듈
+	 * @param string $type 알림메세지가 발생한 대상종류
+	 * @param string $idx 알림메세지가 발생한 대상고유번호
+	 * @param string $code 알림종류
+	 * @param any[] $content 메세지 내용
+	 * @return boolean $success
+	 */
+	function sendPush($target,$module,$type,$idx,$code,$content=array()) {
+		if (is_numeric($target) == true) { // 알림메세지로 전달
+			/**
+			 * 동일한 종류의 알림메세지가 있는지 확인한다.
+			 */
+			$check = $this->db()->select($this->table->push)->where('midx',$target)->where('module',$module)->where('type',$type)->where('idx',$idx)->where('code',$code)->getOne();
 			
+			/**
+			 * 동일한 종류의 알림메세지가 없거나, 알림메세지를 이미 확인한 경우 이전 알림내역을 덮어쓴고,
+			 * 그렇지 않은 경우 내용을 병합한다.
+			 */
+			if ($check == null || $check->is_checked == 'TRUE') {
+				$contents = array();
+			} else {
+				$contents = json_decode($check->contents);
+			}
+			
+			$contents[] = $content;
+			$this->db()->replace($this->table->push,array('midx'=>$target,'module'=>$module,'type'=>$type,'idx'=>$idx,'code'=>$code,'contents'=>json_encode($contents,JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK),'reg_date'=>time(),'is_checked'=>'FALSE'))->execute();
+			
+			return true;
+		} else { // 이메일로 전달
+			/**
+			 * @todo 알림메세지 이메일 전송
+			 */
 		}
 	}
 	
@@ -292,7 +601,7 @@ class ModulePush {
 			}
 		}
 	}
-	
+	/*
 	function sendServer($channel,$data) {
 		$ELEPHANTIO_PATH = $this->Module->getPath().'/classes/elephant.io/src';
 		
@@ -314,8 +623,38 @@ class ModulePush {
 		$EIO->initialize();
 		$EIO->emit('push',array($channel,$data));
 		$EIO->close();
+	}*/
+	
+	/**
+	 * 현재 모듈에서 처리해야하는 요청이 들어왔을 경우 처리하여 결과를 반환한다.
+	 * 소스코드 관리를 편하게 하기 위해 각 요쳥별로 별도의 PHP 파일로 관리한다.
+	 * 작업코드가 '@' 로 시작할 경우 사이트관리자를 위한 작업으로 최고관리자 권한이 필요하다.
+	 *
+	 * @param string $action 작업코드
+	 * @return object $results 수행결과
+	 * @see /process/index.php
+	 */
+	function doProcess($action) {
+		$results = new stdClass();
+		
+		$values = (object)get_defined_vars();
+		$this->IM->fireEvent('beforeDoProcess',$this->getModule()->getName(),$action,$values);
+		
+		/**
+		 * 모듈의 process 폴더에 $action 에 해당하는 파일이 있을 경우 불러온다.
+		 */
+		if (is_file($this->getModule()->getPath().'/process/'.$action.'.php') == true) {
+			INCLUDE $this->getModule()->getPath().'/process/'.$action.'.php';
+		}
+		
+		unset($values);
+		$values = (object)get_defined_vars();
+		$this->IM->fireEvent('afterDoProcess',$this->getModule()->getName(),$action,$values,$results);
+		
+		return $results;
 	}
 	
+	/*
 	function doProcess($action) {
 		$results = new stdClass();
 		$values = new stdClass();
@@ -369,5 +708,6 @@ class ModulePush {
 		
 		return $results;
 	}
+	*/
 }
 ?>
